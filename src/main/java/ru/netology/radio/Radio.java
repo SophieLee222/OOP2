07c3be5b1435042a9bd803d06d168fa822eac3bf
sofia va <sophia.lily.lee@gmail.com>
2024-10-07 19:56:35 +0300

    Code properly adjusted

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,4 +1,4 @@
-package ru.netology;
+package ru.netology.radio;
 
 public class Radio {
     private int currentStation;
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -1,6 +1,7 @@
+package ru.netology.radio;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.netology.Radio;
 
 public class RadioTest {
 
